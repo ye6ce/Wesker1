c781c5bf432e1e68c6feee946ec74e97ddca4e9a
ye6ce <yanisttb5@gmail.com>
2026-04-23 13:41:32 +0100

    Delete WES directory

--- WES/src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../../firebase-applet-config.json';
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth();
